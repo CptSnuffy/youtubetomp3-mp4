c43f94417a8370e0b6fbaa5502f76e26eb93005d
Jake <jhutchinson.programmer@gmail.com>
2022-01-26 21:23:15 -0500

    initial file setup

---
+++ launcher.py
@@ -0,0 +1,8 @@
+from youtubetomp3 import YoutubeToMP3 as youtubemp3
+
+def main():
+    youtubemp3.run
+
+
+if __name__ == "__main__":
+    main
---
+++ youtubetomp3.py
@@ -0,0 +1,12 @@
+
+
+
+class YoutubeToMP3():
+    def __init__(self):
+        pass
+
+    def run(self):
+        self.draw_gui
+
+    def draw_gui(self):
+        print('yeet')
